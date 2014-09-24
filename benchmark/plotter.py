ae5c9d83637a068103f3375f130804fcb2dbedc5
Jan Dreier <jan.dreier@rwth-aachen.de>
2014-09-24 13:59:16 +0200

    plotter.py now deals with json files of the iterative method

--- benchmark/plotter.py
+++ benchmark/plotter.py
@@ -9,20 +9,33 @@ import json
 from optparse import OptionParser
 from pprint import pprint
 
-def plotFile(filename):
+# plot the json-file "filename"
+# plot either the first (round="first") or the last (round="last") round of the iterative method
+def plotFile(filename, round):
     xs = []
     ys = []
     label = ""
     with open(filename) as infile:
         data = json.load(infile)
-        label += "N = " + str(data["_N"])
-        label += ", seed = " + str(data["_seedPercentage"])
-        label += ", maxc = " + str(data["_maxc"])
+
+        i = 0
+        if round == "first":
+            i = 0
+        elif round == "last":
+            i = int(data["_rounds"])-1
+        else:
+            raise Exception("pass either first or last as parameter")
+
+        label += "N=" + str(data["_N"])
+        label += ", seed=" + str(data["_seedFraction"])
+        label += ", maxc=" + str(data["_maxc"])
+        label += ", round=" + str(i)
 
         nmiValuesMean = data["nmiValuesMean"]
         for tup in nmiValuesMean:
+
             xs.append(float(tup["mu"]))
-            ys.append(float(tup["value"]))
+            ys.append(float(tup["value"][i]))
     return xs, ys, label
 
 # interface
@@ -36,10 +49,13 @@ def commandline_interface():
     parser.add_option("-o", dest="output", type="string",
         help="pdf output file")
 
+    parser.add_option("-i", dest="iterative", action="store_true",
+        help="flag without parameter. if set plot first and last value of iterative method")
+
     global options, args
     (options, args) = parser.parse_args()
 
-    if not options.files:
+    if not (options.files and options.output):
         parser.print_help()
         return False
     return True
@@ -52,9 +68,14 @@ if commandline_interface():
     graphs = []
     lines = []
     for f in filenames:
-        xs, ys, label = plotFile(f)
+        xs, ys, label = plotFile(f, "first")
         line, = plt.plot(xs, ys, "-o", label=label)
         lines.append(line)
+
+        if options.iterative:
+            xs, ys, label = plotFile(f, "last")
+            line, = plt.plot(xs, ys, "-o", label=label)
+            lines.append(line)
 
     plt.legend(handles=lines)
     plt.legend(loc="best", fancybox=True, framealpha=0.5)
