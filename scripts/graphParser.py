b2fa1dc27f6090b63953a91b7d393bfca10e5e28
Jan Dreier <jan.dreier@rwth-aachen.de>
2014-09-29 12:28:44 +0200

    put commandline parameters at top op graphParser.py

--- scripts/graphParser.py
+++ scripts/graphParser.py
@@ -10,6 +10,65 @@ from math import ceil
 
 ##########################################
 #
+# COMMAND LINE INTERFACE
+
+
+def commandline_interface():
+    parser = OptionParser()
+   
+    parser.add_option("-g", dest="graph_file_input", type="string",
+            help="Input: LFR benchmark graph file")
+    parser.add_option("-G", dest="graph_file_output", type="string",
+            help="Output: custom graph file")
+
+    parser.add_option("-c", dest="community_file_input", type="string",
+            help="Input: LFR benchmark community network")
+    parser.add_option("-C", dest="community_file_output", type="string",
+            help="Output: custom community file")
+
+    parser.add_option("-s", dest="seed_nodes", type="string",
+            help="Output: custom seed-node file")
+    parser.add_option("-n", dest="seed_frac", type="float",
+            help="fraction of seed nodes (in range 0.0 to 1.0)")
+   
+    global options, args
+    (options, args) = parser.parse_args()
+
+    if not options.graph_file_input:
+        parser.error("input graph file not given")
+        parser.print_help()
+        return False
+
+    elif not options.graph_file_output:
+        parser.error("output graph file not given")
+        parser.print_help()
+        return False
+
+    elif not options.community_file_input:
+        parser.error("input community file not given")
+        parser.print_help()
+        return False
+
+    elif not options.community_file_output:
+        parser.error("output community file not given")
+        parser.print_help()
+        return False
+
+    elif not options.seed_nodes:
+        parser.error("output seed-node file not given")
+        parser.print_help()
+        return False
+
+    elif not hasattr(options, "seed_frac"):
+        parser.error("seed-node fraction not given")
+        parser.print_help()
+        return False
+
+    return True
+
+
+##########################################
+#
 # GRAPH
 #
 # Internally, the graph is represented as a dictionary. 
@@ -133,66 +192,6 @@ def writeSeeds(seeds, communityToVertices, vertexToCommunities, filename):
             for community in vertexToCommunities[seed]:
                 tmp[community] = 1
             f.write("\n" + str(seed) + " " + " ".join([str(x) for x in tmp]))
-
-
-##########################################
-#
-# COMMAND LINE INTERFACE
-
-
-def commandline_interface():
-    usage = "usage: %prog"
-    parser = OptionParser()
-   
-    parser.add_option("-g", dest="graph_file_input", type="string",
-            help="Input: LFR benchmark graph file")
-    parser.add_option("-G", dest="graph_file_output", type="string",
-            help="Output: custom graph file")
-
-    parser.add_option("-c", dest="community_file_input", type="string",
-            help="Input: LFR benchmark community network")
-    parser.add_option("-C", dest="community_file_output", type="string",
-            help="Output: custom community file")
-
-    parser.add_option("-s", dest="seed_nodes", type="string",
-            help="Output: custom seed-node file")
-    parser.add_option("-n", dest="seed_frac", type="float",
-            help="fraction of seed nodes (in range 0.0 to 1.0)")
-   
-    global options, args
-    (options, args) = parser.parse_args()
-
-    if not options.graph_file_input:
-        parser.error("input graph file not given")
-        parser.print_help()
-        return False
-
-    elif not options.graph_file_output:
-        parser.error("output graph file not given")
-        parser.print_help()
-        return False
-
-    elif not options.community_file_input:
-        parser.error("input community file not given")
-        parser.print_help()
-        return False
-
-    elif not options.community_file_output:
-        parser.error("output community file not given")
-        parser.print_help()
-        return False
-
-    elif not options.seed_nodes:
-        parser.error("output seed-node file not given")
-        parser.print_help()
-        return False
-
-    elif not hasattr(options, "seed_frac"):
-        parser.error("seed-node fraction not given")
-        parser.print_help()
-        return False
-
-    return True
 
 
 ##########################################
