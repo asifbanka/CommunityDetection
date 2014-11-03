6f1f28f53cabc2a8cb2c0760039811570fe094dc
Philipp Kuinke <philipp.kuinke@rwth-aachen.de>
2014-11-03 13:51:29 +0100

    fix in job script

--- benchmark/jobs/job_array_overlap.sh
+++ benchmark/jobs/job_array_overlap.sh
@@ -33,37 +33,37 @@ cd $tmpdir
 mkdir -p ../../../../output
 
 case $LSB_JOBINDEX in
-1)  ../benchmark_overlap.py -o ../../../../output -n 1000 -c small -s 0.05 -i 1  -O "0.0 0.5 0.05" --om 3 -m 0.1 --samples_per_datapoint 100 --classification_strategy gap
+1)  ../../benchmark_overlap.py -o ../../../../output -n 1000 -c small -s 0.05 -i 1  -O "0.0 0.5 0.05" --om 3 -m 0.1 --samples_per_datapoint 100 --classification_strategy gap
     ;;
-2)  ../benchmark_overlap.py -o ../../../../output -n 1000 -c small -s 0.10 -i 1  -O "0.0 0.5 0.05" --om 3 -m 0.1 --samples_per_datapoint 100 --classification_strategy gap
+2)  ../../benchmark_overlap.py -o ../../../../output -n 1000 -c small -s 0.10 -i 1  -O "0.0 0.5 0.05" --om 3 -m 0.1 --samples_per_datapoint 100 --classification_strategy gap
     ;;
-3)  ../benchmark_overlap.py -o ../../../../output -n 1000 -c small -s 0.15 -i 1  -O "0.0 0.5 0.05" --om 3 -m 0.1 --samples_per_datapoint 100 --classification_strategy gap
+3)  ../../benchmark_overlap.py -o ../../../../output -n 1000 -c small -s 0.15 -i 1  -O "0.0 0.5 0.05" --om 3 -m 0.1 --samples_per_datapoint 100 --classification_strategy gap
     ;;
-4)  ../benchmark_overlap.py -o ../../../../output -n 1000 -c small -s 0.20 -i 1  -O "0.0 0.5 0.05" --om 3 -m 0.1 --samples_per_datapoint 100 --classification_strategy gap
+4)  ../../benchmark_overlap.py -o ../../../../output -n 1000 -c small -s 0.20 -i 1  -O "0.0 0.5 0.05" --om 3 -m 0.1 --samples_per_datapoint 100 --classification_strategy gap
     ;;
-5)  ../benchmark_overlap.py -o ../../../../output -n 1000 -c small -s 0.05 -i 1  -O "0.0 0.5 0.05" --om 3 -m 0.3 --samples_per_datapoint 100 --classification_strategy gap
+5)  ../../benchmark_overlap.py -o ../../../../output -n 1000 -c small -s 0.05 -i 1  -O "0.0 0.5 0.05" --om 3 -m 0.3 --samples_per_datapoint 100 --classification_strategy gap
     ;;
-6)  ../benchmark_overlap.py -o ../../../../output -n 1000 -c small -s 0.10 -i 1  -O "0.0 0.5 0.05" --om 3 -m 0.3 --samples_per_datapoint 100 --classification_strategy gap
+6)  ../../benchmark_overlap.py -o ../../../../output -n 1000 -c small -s 0.10 -i 1  -O "0.0 0.5 0.05" --om 3 -m 0.3 --samples_per_datapoint 100 --classification_strategy gap
     ;;
-7)  ../benchmark_overlap.py -o ../../../../output -n 1000 -c small -s 0.15 -i 1  -O "0.0 0.5 0.05" --om 3 -m 0.3 --samples_per_datapoint 100 --classification_strategy gap
+7)  ../../benchmark_overlap.py -o ../../../../output -n 1000 -c small -s 0.15 -i 1  -O "0.0 0.5 0.05" --om 3 -m 0.3 --samples_per_datapoint 100 --classification_strategy gap
     ;;
-8)  ../benchmark_overlap.py -o ../../../../output -n 1000 -c small -s 0.20 -i 1  -O "0.0 0.5 0.05" --om 3 -m 0.3 --samples_per_datapoint 100 --classification_strategy gap
+8)  ../../benchmark_overlap.py -o ../../../../output -n 1000 -c small -s 0.20 -i 1  -O "0.0 0.5 0.05" --om 3 -m 0.3 --samples_per_datapoint 100 --classification_strategy gap
     ;;
-9)  ../benchmark_overlap.py -o ../../../../output -n 1000 -c big -s 0.05 -i 1  -O "0.0 0.5 0.05" --om 3 -m 0.1 --samples_per_datapoint 100 --classification_strategy gap
+9)  ../../benchmark_overlap.py -o ../../../../output -n 1000 -c big -s 0.05 -i 1  -O "0.0 0.5 0.05" --om 3 -m 0.1 --samples_per_datapoint 100 --classification_strategy gap
     ;;
-10)  ../benchmark_overlap.py -o ../../../../output -n 1000 -c big -s 0.10 -i 1  -O "0.0 0.5 0.05" --om 3 -m 0.1 --samples_per_datapoint 100 --classification_strategy gap
+10) ../../benchmark_overlap.py -o ../../../../output -n 1000 -c big -s 0.10 -i 1  -O "0.0 0.5 0.05" --om 3 -m 0.1 --samples_per_datapoint 100 --classification_strategy gap
     ;;
-11)  ../benchmark_overlap.py -o ../../../../output -n 1000 -c big -s 0.15 -i 1  -O "0.0 0.5 0.05" --om 3 -m 0.1 --samples_per_datapoint 100 --classification_strategy gap
+11) ../../benchmark_overlap.py -o ../../../../output -n 1000 -c big -s 0.15 -i 1  -O "0.0 0.5 0.05" --om 3 -m 0.1 --samples_per_datapoint 100 --classification_strategy gap
     ;;
-12)  ../benchmark_overlap.py -o ../../../../output -n 1000 -c big -s 0.20 -i 1  -O "0.0 0.5 0.05" --om 3 -m 0.1 --samples_per_datapoint 100 --classification_strategy gap
+12) ../../benchmark_overlap.py -o ../../../../output -n 1000 -c big -s 0.20 -i 1  -O "0.0 0.5 0.05" --om 3 -m 0.1 --samples_per_datapoint 100 --classification_strategy gap
     ;;
-13)  ../benchmark_overlap.py -o ../../../../output -n 1000 -c big -s 0.05 -i 1  -O "0.0 0.5 0.05" --om 3 -m 0.3 --samples_per_datapoint 100 --classification_strategy gap
+13) ../../benchmark_overlap.py -o ../../../../output -n 1000 -c big -s 0.05 -i 1  -O "0.0 0.5 0.05" --om 3 -m 0.3 --samples_per_datapoint 100 --classification_strategy gap
     ;;
-14)  ../benchmark_overlap.py -o ../../../../output -n 1000 -c big -s 0.10 -i 1  -O "0.0 0.5 0.05" --om 3 -m 0.3 --samples_per_datapoint 100 --classification_strategy gap
+14) ../../benchmark_overlap.py -o ../../../../output -n 1000 -c big -s 0.10 -i 1  -O "0.0 0.5 0.05" --om 3 -m 0.3 --samples_per_datapoint 100 --classification_strategy gap
     ;;
-15)  ../benchmark_overlap.py -o ../../../../output -n 1000 -c big -s 0.15 -i 1  -O "0.0 0.5 0.05" --om 3 -m 0.3 --samples_per_datapoint 100 --classification_strategy gap
+15) ../../benchmark_overlap.py -o ../../../../output -n 1000 -c big -s 0.15 -i 1  -O "0.0 0.5 0.05" --om 3 -m 0.3 --samples_per_datapoint 100 --classification_strategy gap
     ;;
-16)  ../benchmark_overlap.py -o ../../../../output -n 1000 -c big -s 0.20 -i 1  -O "0.0 0.5 0.05" --om 3 -m 0.3 --samples_per_datapoint 100 --classification_strategy gap
+16) ../../benchmark_overlap.py -o ../../../../output -n 1000 -c big -s 0.20 -i 1  -O "0.0 0.5 0.05" --om 3 -m 0.3 --samples_per_datapoint 100 --classification_strategy gap
     ;;
 *)  echo "error"
     ;;
