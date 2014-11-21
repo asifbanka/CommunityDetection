4afd8c116cc81fc0d17be41d7ce4e48b7133c44c
Jan Dreier <jan.dreier@rwth-aachen.de>
2014-11-21 11:08:00 +0100

    add plots to paper

--- benchmark/makeallplots.sh
+++ benchmark/makeallplots.sh
@@ -1,31 +1,50 @@
 #!/usr/bin/env zsh
 
-./plotter.py -f "$(ls benchmarkoutput/nonoverlap_noniter/1000*small*)"     -m nonoverlap -i 0 -o plots/nonoverlap_noniter_1000_small.pdf
-./plotter.py -f "$(ls benchmarkoutput/nonoverlap_noniter/5000*small*)"     -m nonoverlap -i 0 -o plots/nonoverlap_noniter_5000_small.pdf
-./plotter.py -f "$(ls benchmarkoutput/nonoverlap_noniter/1000*big*)"       -m nonoverlap -i 0 -o plots/nonoverlap_noniter_1000_big.pdf
-./plotter.py -f "$(ls benchmarkoutput/nonoverlap_noniter/5000*big*)"       -m nonoverlap -i 0 -o plots/nonoverlap_noniter_5000_big.pdf
+# transforms json files to pdfs
+# call like this:
+# makeallplots.sh path/to/json/files /outputfolder/for/pdfs
 
-./plotter.py -f "$(ls benchmarkoutput/nonoverlap_iter/1000*small*)"        -m nonoverlap -i 9 -o plots/nonoverlap_iter_1000_small.pdf
-./plotter.py -f "$(ls benchmarkoutput/nonoverlap_iter/5000*small*)"        -m nonoverlap -i 9 -o plots/nonoverlap_iter_5000_small.pdf
-./plotter.py -f "$(ls benchmarkoutput/nonoverlap_iter/1000*big*)"          -m nonoverlap -i 9 -o plots/nonoverlap_iter_1000_big.pdf
-./plotter.py -f "$(ls benchmarkoutput/nonoverlap_iter/5000*big*)"          -m nonoverlap -i 9 -o plots/nonoverlap_iter_5000_big.pdf
+DIR=${0%/*}
+plotter=$DIR/plotter.py
 
-./plotter.py -f "$(ls benchmarkoutput/overlap_noniter/1000*small*0.1mu*)"  -m overlap    -i 0 -o plots/overlap_noniter_0.1mu_1000_small.pdf
-./plotter.py -f "$(ls benchmarkoutput/overlap_noniter/5000*small*0.1mu*)"  -m overlap    -i 0 -o plots/overlap_noniter_0.1mu_5000_small.pdf
-./plotter.py -f "$(ls benchmarkoutput/overlap_noniter/1000*big*0.1mu*)"    -m overlap    -i 0 -o plots/overlap_noniter_0.1mu_1000_big.pdf
-./plotter.py -f "$(ls benchmarkoutput/overlap_noniter/5000*big*0.1mu*)"    -m overlap    -i 0 -o plots/overlap_noniter_0.1mu_5000_big.pdf
+indir=$1
+outdir=$2
 
-./plotter.py -f "$(ls benchmarkoutput/overlap_noniter/1000*small*0.3mu*)"  -m overlap    -i 0 -o plots/overlap_noniter_0.3mu_1000_small.pdf
-./plotter.py -f "$(ls benchmarkoutput/overlap_noniter/5000*small*0.3mu*)"  -m overlap    -i 0 -o plots/overlap_noniter_0.3mu_5000_small.pdf
-./plotter.py -f "$(ls benchmarkoutput/overlap_noniter/1000*big*0.3mu*)"    -m overlap    -i 0 -o plots/overlap_noniter_0.3mu_1000_big.pdf
-./plotter.py -f "$(ls benchmarkoutput/overlap_noniter/5000*big*0.3mu*)"    -m overlap    -i 0 -o plots/overlap_noniter_0.3mu_5000_big.pdf
+iterations=9
 
-./plotter.py -f "$(ls benchmarkoutput/overlap_iter/1000*small*0.1mu*)"     -m overlap    -i 9 -o plots/overlap_iter_0.1mu_1000_small.pdf
-./plotter.py -f "$(ls benchmarkoutput/overlap_iter/5000*small*0.1mu*)"     -m overlap    -i 9 -o plots/overlap_iter_0.1mu_5000_small.pdf
-./plotter.py -f "$(ls benchmarkoutput/overlap_iter/1000*big*0.1mu*)"       -m overlap    -i 9 -o plots/overlap_iter_0.1mu_1000_big.pdf
-./plotter.py -f "$(ls benchmarkoutput/overlap_iter/5000*big*0.1mu*)"       -m overlap    -i 9 -o plots/overlap_iter_0.1mu_5000_big.pdf
+$plotter -f "$(ls $indir/nonoverlap_noniter/1000*small*)"     -m nonoverlap -i 0           -o $outdir/nonoverlap_noniter_a.pdf
+$plotter -f "$(ls $indir/nonoverlap_noniter/1000*big*)"       -m nonoverlap -i 0           -o $outdir/nonoverlap_noniter_b.pdf
+$plotter -f "$(ls $indir/nonoverlap_noniter/5000*small*)"     -m nonoverlap -i 0           -o $outdir/nonoverlap_noniter_c.pdf
+$plotter -f "$(ls $indir/nonoverlap_noniter/5000*big*)"       -m nonoverlap -i 0           -o $outdir/nonoverlap_noniter_d.pdf
 
-./plotter.py -f "$(ls benchmarkoutput/overlap_iter/1000*small*0.3mu*)"     -m overlap    -i 9 -o plots/overlap_iter_0.3mu_1000_small.pdf
-./plotter.py -f "$(ls benchmarkoutput/overlap_iter/5000*small*0.3mu*)"     -m overlap    -i 9 -o plots/overlap_iter_0.3mu_5000_small.pdf
-./plotter.py -f "$(ls benchmarkoutput/overlap_iter/1000*big*0.3mu*)"       -m overlap    -i 9 -o plots/overlap_iter_0.3mu_1000_big.pdf
-./plotter.py -f "$(ls benchmarkoutput/overlap_iter/5000*big*0.3mu*)"       -m overlap    -i 9 -o plots/overlap_iter_0.3mu_5000_big.pdf
+$plotter -f "$(ls $indir/nonoverlap_iter/1000*small*)"        -m nonoverlap -i $iterations -o $outdir/nonoverlap_iter_a.pdf
+$plotter -f "$(ls $indir/nonoverlap_iter/1000*big*)"          -m nonoverlap -i $iterations -o $outdir/nonoverlap_iter_b.pdf
+$plotter -f "$(ls $indir/nonoverlap_iter/5000*small*)"        -m nonoverlap -i $iterations -o $outdir/nonoverlap_iter_c.pdf
+$plotter -f "$(ls $indir/nonoverlap_iter/5000*big*)"          -m nonoverlap -i $iterations -o $outdir/nonoverlap_iter_d.pdf
+
+$plotter -f "$(ls $indir/overlap_noniter/1000*small*0.1mu*)"  -m overlap    -i 0           -o $outdir/overlap_noniter_1mu_a.pdf
+$plotter -f "$(ls $indir/overlap_noniter/1000*big*0.1mu*)"    -m overlap    -i 0           -o $outdir/overlap_noniter_1mu_b.pdf
+$plotter -f "$(ls $indir/overlap_noniter/5000*small*0.1mu*)"  -m overlap    -i 0           -o $outdir/overlap_noniter_1mu_c.pdf
+$plotter -f "$(ls $indir/overlap_noniter/5000*big*0.1mu*)"    -m overlap    -i 0           -o $outdir/overlap_noniter_1mu_d.pdf
+
+$plotter -f "$(ls $indir/overlap_noniter/1000*small*0.3mu*)"  -m overlap    -i 0           -o $outdir/overlap_noniter_3mu_a.pdf
+$plotter -f "$(ls $indir/overlap_noniter/1000*big*0.3mu*)"    -m overlap    -i 0           -o $outdir/overlap_noniter_3mu_b.pdf
+$plotter -f "$(ls $indir/overlap_noniter/5000*small*0.3mu*)"  -m overlap    -i 0           -o $outdir/overlap_noniter_3mu_c.pdf
+$plotter -f "$(ls $indir/overlap_noniter/5000*big*0.3mu*)"    -m overlap    -i 0           -o $outdir/overlap_noniter_3mu_d.pdf
+
+$plotter -f "$(ls $indir/overlap_iter/1000*small*0.1mu*)"     -m overlap    -i $iterations -o $outdir/overlap_iter_1mu_a.pdf
+$plotter -f "$(ls $indir/overlap_iter/1000*big*0.1mu*)"       -m overlap    -i $iterations -o $outdir/overlap_iter_1mu_b.pdf
+$plotter -f "$(ls $indir/overlap_iter/5000*small*0.1mu*)"     -m overlap    -i $iterations -o $outdir/overlap_iter_1mu_c.pdf
+$plotter -f "$(ls $indir/overlap_iter/5000*big*0.1mu*)"       -m overlap    -i $iterations -o $outdir/overlap_iter_1mu_d.pdf
+
+$plotter -f "$(ls $indir/overlap_iter/1000*small*0.3mu*)"     -m overlap    -i $iterations -o $outdir/overlap_iter_3mu_a.pdf
+$plotter -f "$(ls $indir/overlap_iter/1000*big*0.3mu*)"       -m overlap    -i $iterations -o $outdir/overlap_iter_3mu_b.pdf
+$plotter -f "$(ls $indir/overlap_iter/5000*small*0.3mu*)"     -m overlap    -i $iterations -o $outdir/overlap_iter_3mu_c.pdf
+$plotter -f "$(ls $indir/overlap_iter/5000*big*0.3mu*)"       -m overlap    -i $iterations -o $outdir/overlap_iter_3mu_d.pdf
+
+
+$plotter -f $indir/nonoverlap_iter/1000N_bigCommunities_0on_0om_0.02seed.json  -m nonoverlap -i "0 $iterations" -o $outdir/nonoverlap_compare_a.pdf
+$plotter -f $indir/nonoverlap_iter/5000N_bigCommunities_0on_0om_0.06seed.json  -m nonoverlap -i "0 $iterations" -o $outdir/nonoverlap_compare_b.pdf
+
+$plotter -f $indir/overlap_iter/1000N_smallCommunities_0.1mu_3om_0.04seed.json -m overlap    -i "0 $iterations" -o $outdir/overlap_compare_a.pdf
+$plotter -f $indir/overlap_iter/5000N_bigCommunities_0.1mu_3om_0.06seed.json   -m overlap    -i "0 $iterations" -o $outdir/overlap_compare_b.pdf
